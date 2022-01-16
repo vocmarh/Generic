cf2635fb28be5884c83e88bf8f8083ecb2eaaae7
Sergei Khramtsov <vocmarh1989@gmail.com>
2022-01-16 11:42:20 +0500

    Add project files.

---
+++ Generic/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account<int> account1 = new Account<int>();
+            account1.input<int>(1000);
+
+            Account<string> account2 = new Account<string>();
+            account2.input<string>("105а");
+
+            Console.ReadKey();
+        }
+
+        class Account<T>
+        {
+            private T accountNumber { get; set; }
+            private double accountBalance = 1000;
+            private string accountName { get; set; }
+
+            public void input<T>(T value)
+            {
+                Console.WriteLine("Введите номер счета: {0}", value);
+                Console.Write("Введите баланс: ");
+                Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите ФИО: ");
+                Convert.ToString(Console.ReadLine());
+            }
+        }
+    }
+}
